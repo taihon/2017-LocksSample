c7bffda1dbdc46e1824ab8a6f177ce829780f232
taihon <taihon@users.noreply.github.com>
2017-12-25 00:45:49 +0700

    added base objects for storing permissions in db

---
+++ TasksManager.Entities/ObjectPermission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TasksManager.Entities
+{
+    public class ObjectPermission:DomainObject
+    {
+        public int ObjectId { get; set; }
+        public string ObjectType { get; set; }
+        public int UserId { get; set; }
+        public PermissionType PermissionType { get; set; }
+    }
+}
---
+++ TasksManager.Entities/PermissionType.cs
@@ -0,0 +1,7 @@
+﻿namespace TasksManager.Entities
+{
+    public class PermissionType:DomainObject
+    {
+        public string Name { get; set; }
+    }
+}
